45849604571031f03bf3e76e7ec51d1576405ebb
Danilo Gutierrez <danilo1905@outlook.com>
2022-11-15 13:17:36 -0500

    add styles to output zone

--- src/components/OutputArea/styles.tsx
+++ src/components/OutputArea/styles.tsx
@@ -2,8 +2,13 @@ import styled from 'styled-components'
 
 export const Aside = styled.aside`
   width: 50%;
-  height: 95vh;
+  height: 94vh;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background: #269fba3b;
+  padding: 0 2rem;
+  border-radius: 0.4rem;
+  overflow-y: scroll;
+  overflow-wrap: break-word;
 
   & h1 {
     font-size: 1.8rem;
